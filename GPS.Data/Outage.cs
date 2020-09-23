c53f494e24fc5fc63111ffc3e212acafbf7a0297
braydenperry <braydenperry1@mail.weber.edu>
2020-09-23 12:07:04 -0600

    Rolled start and end attributes into DateTime attributes

--- GPS.Data/Outage.cs
+++ GPS.Data/Outage.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Xml.Serialization;
-using GPS.Data.ParserObjects;
 
 namespace GPS.Data
 {
@@ -16,43 +15,15 @@ namespace GPS.Data
 		[XmlAttribute("SVN")]
 		public string SatelliteVehicleNumber { get; set; }
 
-		[XmlAttribute("NAME")]
 		public string Name { get; set; }
 
-		[XmlAttribute("TYPE")]
 		public string Type { get; set; }
 
 		[XmlAttribute("REFERENCE")]
 		public string Reference { get; set; }
 
-		[XmlAttribute("START_YEAR")]
-		public string StartYear { get; set; }
+		public DateTime StartTime { get; set; }
 
-		[XmlAttribute("START_DOY")]
-		public string StartDayOfYear { get; set; }
-
-		[XmlAttribute("START_HR")]
-		public string StartHour { get; set; }
-
-		[XmlAttribute("START_MIN")]
-		public string StartMinute { get; set; }
-
-		[XmlAttribute("START_SEC")]
-		public string StartSecond { get; set; }
-
-		[XmlAttribute("END_YEAR")]
-		public string EndYear { get; set; }
-
-		[XmlAttribute("END_DOY")]
-		public string EndDayOfYear { get; set; }
-
-		[XmlAttribute("END_HR")]
-		public string EndHour { get; set; }
-
-		[XmlAttribute("END_MIN")]
-		public string EndMinute { get; set; }
-
-		[XmlAttribute("END_SEC")]
-		public string EndSecond { get; set; }
+		public DateTime EndTime { get; set; }
 	}
 }
