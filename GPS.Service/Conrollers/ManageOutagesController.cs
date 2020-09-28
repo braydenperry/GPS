5e53d266408ae3a55a7d9ba4ffc31ea830a73a98
braydenperry <braydenperry1@mail.weber.edu>
2020-09-28 15:47:32 -0600

    Upload via endpoints working.

---
+++ GPS.Service/Conrollers/ManageOutagesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GPS.Service.Conrollers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ManageOutagesController : ControllerBase
+    {
+        public static IWebHostEnvironment _environment;
+
+        public ManageOutagesController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+        
+        public class FileUpload
+        {
+            public IFormFile File { get; set; }
+        }
+        
+        [HttpPost]
+        public async Task<string> Post([FromForm] FileUpload fileUpload)
+        {
+            try
+            {
+                if (fileUpload.File.Length > 0) // Make sure there's actually a file
+                {
+                    using FileStream fileStream = System.IO.File.Create(_environment.WebRootPath + "\\SOF\\" + fileUpload.File.FileName);
+                    await fileUpload.File.CopyToAsync(fileStream);
+                    fileStream.Flush();
+                    return "File uploaded.";
+                }
+                else
+                {
+                    return "File failed to upload.";
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message.ToString();
+            }
+        }
+
+    }
+}
