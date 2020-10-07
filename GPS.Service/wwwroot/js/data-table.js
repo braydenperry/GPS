2905df62397e793e5289c20cb0973b0d583ff6d0
tannerp6260 <tannerp6260@gmail.com>
2020-10-07 16:20:48 -0600

    Moved closing tags for 'tbody' and 'tfoot' to correct locations; removed unnecessary text from page; changed table tfoot text to match thead text

--- GPS.Service/wwwroot/js/data-table.js
+++ GPS.Service/wwwroot/js/data-table.js
@@ -3,17 +3,3 @@
     responsive: true
     //searching: false
 });
-
-$('.mydatatable tfoot th').each(function () {
-    var title = $(this).text();
-    $(this).html('<input type="text" placeholder="Search ' + title + '" />');
-});
-
-table.columns().every(function () {
-    var that = this;
-    $('input', this.footer()).on('keyup change', function () {
-        if (that.search() !== this.value) {
-            that.search(this.value).draw();
-        }
-    })
-})
