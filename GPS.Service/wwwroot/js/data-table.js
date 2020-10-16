3630f1dea37adb980ae06f2585ac35cf356508af
tannerp6260 <tannerp6260@gmail.com>
2020-10-16 13:13:32 -0600

    Eddited data-table.js during merge.

--- GPS.Service/wwwroot/js/data-table.js
+++ GPS.Service/wwwroot/js/data-table.js
@@ -1,5 +1,20 @@
 ﻿var table = $('.mydatatable').DataTable({
     lengthMenu: [[5, 10, 25, 50, 100, -1], [5, 10, 25, 50, 100, "All"]],
-    responsive: true
     //searching: false
 });
+
+//searching box
+$('.mydatatable tfoot th').each(function () {
+    var title = $(this).text();
+    $(this).html('<input type="text" placeholder="Search" />');
+});
+
+//search functionality
+table.columns().every(function () {
+    var that = this;
+    $('input', this.footer()).on('keyup change', function () {
+        if (that.search() !== this.value) {
+            that.search(this.value).draw();
+        }
+    })
+})
