b33d312cb68c420243445b074a84e6f2b85d92ec
braydenperry <braydenperry1@mail.weber.edu>
2020-10-19 10:23:39 -0600

    Validates that the uploaded file has .sof extension. Deletes the SOF file if it already exists when a new one is uploaded.

--- GPS.Service/Conrollers/ManageOutagesController.cs
+++ GPS.Service/Conrollers/ManageOutagesController.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -15,9 +13,12 @@ namespace GPS.Service.Conrollers
     {
         public static IWebHostEnvironment _environment;
 
+        private readonly string _filePath;
+
         public ManageOutagesController(IWebHostEnvironment environment)
         {
             _environment = environment;
+            _filePath = _environment.WebRootPath + "\\SOF\\current.sof";
         }
         
         public class FileUpload
@@ -28,24 +29,43 @@ namespace GPS.Service.Conrollers
         [HttpPost]
         public async Task<string> Post([FromForm] FileUpload file)
         {
+
             try
             {
+
                 if (file.File.Length > 0) // Make sure there's actually a file
                 {
-                    using FileStream fileStream = System.IO.File.Create(_environment.WebRootPath + "\\SOF\\" + file.File.FileName);
-                    await file.File.CopyToAsync(fileStream);
-                    fileStream.Flush();
-                    return "File uploaded.";
+                    var newFilePath = _environment.WebRootPath + "\\SOF\\" + file.File.FileName;
+
+                    if (SOFExists())
+                    {
+                        Delete();
+                    }
+                    
+                    if (ValidExtension(newFilePath))
+                    {
+                        using FileStream fileStream = System.IO.File.Create(newFilePath);
+                        await file.File.CopyToAsync(fileStream);
+                        fileStream.Flush();
+                        return "File uploaded.";
+                    }
+                    else
+                    {
+                        return "Invalid file type.";
+                    }
+    
                 }
                 else
                 {
                     return "File failed to upload.";
                 }
+
             }
             catch (Exception ex)
             {
                 return ex.Message.ToString();
             }
+
         }
 
         [HttpDelete]
@@ -55,25 +75,52 @@ namespace GPS.Service.Conrollers
             try
             {
 
-                var sofPath = _environment.WebRootPath + "\\SOF\\current.sof";
-
-                if (System.IO.File.Exists(sofPath))
+                if (SOFExists())
                 {
-                    System.IO.File.Delete(sofPath);
+                    System.IO.File.Delete(_filePath);
                 }
                 else
                 {
                     return "SOF file does not exist.";
                 }
 
+                return "Delete successful";
             }
             catch (Exception ex)
             {
                 return ex.Message.ToString();
             }
+            
+        }
 
-            return "Delete successful";
+        private bool SOFExists()
+        {
+
+            if (System.IO.File.Exists(_filePath))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
 
         }
+
+        private bool ValidExtension(string filePath)
+        {
+            string extension = Path.GetExtension(filePath);
+            
+            if (extension.ToLower() == ".sof")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
     }
 }
