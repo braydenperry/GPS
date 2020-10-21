b1ec6aa34ff3fc3daaa5a8a969fa208efce19090
tannerp6260 <tannerp6260@gmail.com>
2020-10-21 17:02:44 -0600

    Removed commented segment of code.

--- GPS.Service/wwwroot/js/data-table.js
+++ GPS.Service/wwwroot/js/data-table.js
@@ -9,24 +9,6 @@ $('.mydatatable tfoot th').each(function () {
     $(this).html('<input type="text" placeholder="Search" />');
 });
 
-////search functionality
-//table.columns().every(function () {
-//    var that = this;
-//    $('input', this.footer()).on('keyup change', function () {
-//        //create a special variable using .escapeRegex function
-//        //see https://datatables.net/reference/api/$.fn.dataTable.util.escapeRegex()
-//        var val = $.fn.dataTable.util.escapeRegex(
-//            $(this).val()
-//        );
-//        //"val ? '^' + val : ''" expression functions as the string argument in 'search()' function
-//        //'^' in a regular expression means the tested string must START with the respective string
-//        if (that.search(val ? '^' + val : '', true, false)) {
-//            that.draw();
-//        }
-//    })
-//})
-
-
 table.columns().indexes().flatten().each(function (i) {
     var column = table.column(i);
     // Dropdown columns ('Tag Name' and 'Type')
