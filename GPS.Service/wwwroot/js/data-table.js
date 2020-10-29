9593a726cf3c894aa0f5801e4adda6d09734e493
tannerp6260 <tannerp6260@gmail.com>
2020-10-29 16:40:46 -0600

    Fixed typo

--- GPS.Service/wwwroot/js/data-table.js
+++ GPS.Service/wwwroot/js/data-table.js
@@ -5,6 +5,9 @@
     let table = $('.mydatatable').DataTable({
         lengthMenu: [[5, 10, 25, 50, 100, -1], [5, 10, 25, 50, 100, "All"]],
         //searching: false
+        "search": {
+            "smart": true
+        }
     });
 
     //searching box
