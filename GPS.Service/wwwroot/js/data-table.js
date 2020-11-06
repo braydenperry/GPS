52b389abdb8f5c03d7e4a034d846cb2eaf5978b4
tannerp6260 <tannerp6260@gmail.com>
2020-11-05 19:32:46 -0700

    Modified code to correctly implement footer search and dropdown filters to run through the api.

--- GPS.Service/wwwroot/js/data-table.js
+++ GPS.Service/wwwroot/js/data-table.js
@@ -1,4 +1,11 @@
 ﻿var localDataTable;
+const Tag_Name = 0;
+const ID = 1;
+const SVN = 2; 
+const Start_Time = 3;
+const End_Time = 4;
+const Type = 5;
+const Reference = 6;
 
 $(document).ready(function () {
     loadList();
@@ -26,64 +33,50 @@ function loadList() {
             "emptyTable": "no data found."
         },
         "width": "100%",
-        "order": [[6, 'asc']]
+        "order": [[6, 'asc']],
+
+        initComplete: function () {
+            this.api().columns([Tag_Name, Type]).every(function () {
+                var column = this;
+                var select = $('<select><option value=""></option></select>')
+                    .appendTo($(column.footer()).empty())
+                    .on('change', function () {
+                        var val = $.fn.dataTable.util.escapeRegex(
+                            $(this).val()
+                        );
+
+                        column
+                            .search(val ? '^' + val + '$' : '', true, false)
+                            .draw();
+                    });
+
+                column.data().unique().sort().each(function (d, j) {
+                    select.append('<option value="' + d + '">' + d + '</option>')
+                });
+            });
+
+            this.api().columns([ID, SVN, Start_Time, Type]).every(function () {
+                localDataTable.columns().every(function () {
+                var column = this;
+                    $('input', this.footer()).on('keyup change', function () {
+                        var val = $.fn.dataTable.util.escapeRegex(
+                            $(this).val()
+                        );
+                        if (column.search(val ? '^' + val : '', true, false)) {
+                            column.draw();
+                        }
+                    })
+                })
+            });
+        }
     });
 }
 
-
-
-/*
- *Below is the old data-table.js code that was previously working.
- *With this code included everything continues to function properly except for the dropdown menus
-*/
 $('.mydatatable').ready(function () {
 
     $.fn.dataTable.moment('M/D/YYYY HH:mm:ss');
 
     $('.mydatatable tfoot th').each(function () {
         $(this).html('<input type="text" placeholder="Search" />');
-    });
-
-    localDataTable.columns().indexes().flatten().each(function (column_index) {
-        var column = localDataTable.column(column_index);
-        const Tag_Name = 0;
-        const Type = 5;
-        // Dropdown columns ('Tag Name' and 'Type')
-        if (column_index === Tag_Name | column_index === Type) {
-            var select = $('<select><option value=""></option></select>')
-                .appendTo($(column.footer()).empty())
-                .on('change', function () {
-                    // Escape the expression so we can perform a regex match
-                    var val = $.fn.dataTable.util.escapeRegex(
-                        $(this).val()
-                    );
-
-                    column
-                        .search(val ? '^' + val + '$' : '', true, false)
-                        .draw();
-
-                });
-            column.data().unique().sort().each(function (d, j) {
-                select.append('<option value="' + d + '">' + d + '</option>')
-            });
-        }
-        // Search columns
-        else {
-            localDataTable.columns().every(function () {
-                var that = this;
-                $('input', this.footer()).on('keyup change', function () {
-                    //create a special variable using .escapeRegex function
-                    //see https://datatables.net/reference/api/$.fn.dataTable.util.escapeRegex()
-                    var val = $.fn.dataTable.util.escapeRegex(
-                        $(this).val()
-                    );
-                    //"val ? '^' + val : ''" expression functions as the string argument in 'search()' function
-                    //'^' in a regular expression means the tested string must START with the respective string
-                    if (that.search(val ? '^' + val : '', true, false)) {
-                        that.draw();
-                    }
-                })
-            })
-        }
     });
 });
