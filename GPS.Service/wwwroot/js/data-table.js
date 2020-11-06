0dd8eeaa80f142d72a36e6c337f59c535010bb10
tannerp6260 <tannerp6260@gmail.com>
2020-11-05 18:28:05 -0700

    Specified "Reference" column as initially sorted in ascending order per Conner's request.

--- GPS.Service/wwwroot/js/data-table.js
+++ GPS.Service/wwwroot/js/data-table.js
@@ -25,7 +25,8 @@ function loadList() {
         "language": {
             "emptyTable": "no data found."
         },
-        "width": "100%"
+        "width": "100%",
+        "order": [[6, 'asc']]
     });
 }
 
