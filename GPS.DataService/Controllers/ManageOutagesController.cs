359a25866303b7f90c197c57e5fb670707d9325a
connorwood2 <70246684+connorwood2@users.noreply.github.com>
2020-11-18 15:14:17 -0700

    Revert "Merge branch 'Develop' of https://github.com/braydenperry/GPS into Develop"
    This reverts commit 293c1a30b0812a4d030816a1a78ed057b301d54f, reversing
    changes made to a5db562168e276dcbf90ac8714247e2b04c7a87c.

--- GPS.DataService/Controllers/ManageOutagesController.cs
+++ GPS.DataService/Controllers/ManageOutagesController.cs
@@ -5,56 +5,62 @@ using System;
 
 namespace GPS.DataService.Controllers
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class ManageOutagesController : Controller
-	{
-		#region Properties
-		public readonly IOutageRepository _outageRepository;
-		#endregion
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ManageOutagesController : Controller
+    {
+        #region Properties
+        public readonly IOutageRepository _outageRepository;
+        #endregion
 
-		#region Constructor
-		public ManageOutagesController(IOutageRepository outageRepository)
-		{
-			_outageRepository = outageRepository;
-		}
-		#endregion
+        #region Constructor
+        public ManageOutagesController(IOutageRepository outageRepository)
+        {
+            _outageRepository = outageRepository;
+        }
+        #endregion
 
-		#region HTTP Methods
-		[HttpPost]
-		public IActionResult Post(IFormFile file)
-		{
-			try
-			{
-				if (file != null)
-				{
-					_outageRepository.Upload(file.OpenReadStream());
-					return StatusCode(202, "File Uploaded");
-				}
-				else
-				{
-					throw new ArgumentNullException();
-				}
-			}
-			catch (Exception)
-			{
-				return StatusCode(400);
-			}
-		}
+        #region HTTP Methods
+        [HttpPost]
+        public IActionResult Post(IFormFile file)
+        {
 
-		[HttpDelete]
-		public IActionResult Delete()
-		{
-			try
-			{
-				_outageRepository.Delete();
-				return StatusCode(202, "File Deleted");
-			}
-			catch (Exception)
-			{
-				return StatusCode(400);
-			}
-		}
-		#endregion
-	}
+            try
+            {
+
+                if (file != null)
+                {
+                    _outageRepository.Upload(file.OpenReadStream());
+                    return StatusCode(202, "File Uploaded");
+                }
+                else
+                {
+                    throw new ArgumentNullException();
+                }
+
+            }
+            catch (Exception)
+            {
+                return StatusCode(400);
+            }
+
+        }
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+
+            try
+            {
+                _outageRepository.Delete();
+                return StatusCode(202, "File Deleted");
+            }
+            catch (Exception)
+            {
+                return StatusCode(400);
+            }
+
+        }
+        #endregion
+    }
 }
---
+++ GPS.Service/Pages/SOFDownload.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace GPS.WebApp.Pages
+{
+	public class SOFDownloadModel : PageModel
+	{
+		private readonly ILogger<SOFDownloadModel> _logger;
+
+		public SOFDownloadModel(ILogger<SOFDownloadModel> logger)
+		{
+			_logger = logger;
+		}
+
+		public void OnGet()
+		{
+	
+		}
+	}
+}
